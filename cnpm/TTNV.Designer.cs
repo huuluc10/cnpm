073aa64f524c6a5e8bc27cce41dd78787f69fa0f
8ivlvi8 <letam2002pk@gmail.com>
2023-04-23 12:09:02 +0700

    sửa TTNV

--- cnpm/TTNV.Designer.cs
+++ cnpm/TTNV.Designer.cs
@@ -33,6 +33,7 @@
             this.textBox_accNV = new System.Windows.Forms.TextBox();
             this.label_passNV = new System.Windows.Forms.Label();
             this.textBox_passNV = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // labelTTNV
@@ -80,12 +81,22 @@
             this.textBox_passNV.Size = new System.Drawing.Size(262, 20);
             this.textBox_passNV.TabIndex = 5;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(166, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Sửa";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // TTNV
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(688, 329);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox_passNV);
             this.Controls.Add(this.label_passNV);
             this.Controls.Add(this.textBox_accNV);
@@ -106,5 +117,6 @@
         private System.Windows.Forms.TextBox textBox_accNV;
         private System.Windows.Forms.Label label_passNV;
         private System.Windows.Forms.TextBox textBox_passNV;
+        private System.Windows.Forms.Button button1;
     }
 }
